f7af813817c027fee9c0d3622dac9bb29e0df176
maojianduo <maojianduo@foxmail.com>
2020-08-28 12:46:00 +0800

    init project

---
+++ jksj-current-program/src/main/java/com/damaojd/booknote/jksj_current_program/App.java
@@ -0,0 +1,13 @@
+package com.damaojd.booknote.jksj_current_program;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
